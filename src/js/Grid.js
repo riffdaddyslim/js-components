da863ede741e643c758a1edac914f9dfd44b8f38
Tyler Riffle <riffletyler3@gmail.com>
2022-06-07 12:59:40 -0600

    Added ability for rows to have expand for additional data

--- src/js/Grid.js
+++ src/js/Grid.js
@@ -10,10 +10,11 @@ import Component from "./Component.js"
 /**
  * @memberof DataGrid
  * @typedef {Object} ColumnItem Object used to define params for each grid column
- * @property {String} key Identifier for the column
+ * @property {String} [key=null] Identifier for the column
  * @property {Function} [content=value for key] Function to create content given row data
  * @property {String} [display=all caps of the key] String to use as the header
  * @property {String} [width=1fr] Determines width of the column
+ * @property {String} [id=null] Used internally to identify programatically added columns
  */
 
 class Grid extends Component {
@@ -30,14 +31,19 @@ class Grid extends Component {
     #columns = null
     #data = null
 
+    // Optional Params
+    #expand
+
     /**
      * JS Grid Component
      * @param {HTMLElement} container Element to build the grid
      * @param {ColumnItem[]} columns 
      * @param {Object} data Data to display in the grid
+     * @param {Function} expand Function used to display expand data
      * @param {Boolean} numbered Determines if the rows will be numbered
      */
     constructor(container, columns, data, {
+        expand = null,
         numbered = false
     } = {}) {
         super({ container })
@@ -48,9 +54,18 @@ class Grid extends Component {
         // Adds numbered column
         if (numbered) {
             this.#columns.unshift({
-                key: "#",
+                display: "#",
                 content: (rowData, index) => index + 1,
                 width: "50px"
+            })
+        }
+
+        this.#expand = expand
+        if (this.#expand) {
+            this.#columns.unshift({
+                content: () => `<span class="expandIcon"></span>`,
+                width: "50px",
+                id: "expand"
             })
         }
     }
@@ -68,8 +83,9 @@ class Grid extends Component {
     }
 
     #getHeaderDisplay(column) {
-        if (!column.display) return column.key.toUpperCase()
-        return column.display
+        if (column.display) return column.display
+        if (column.key) return column.key.toUpperCase()
+        return ""
     }
 
     #renderHeader() {
@@ -91,20 +107,41 @@ class Grid extends Component {
         this.container.setAttribute("style", layout)
     }
 
+    #getCellContent(index, row, column) {
+        let content = column.content ? column.content(row, index) : row[column.key] ?? "&mdash;"
+
+        // Removed icon for expand if expand function returns false
+        if (column.id === "expand" && !this.#expand(row)) content = "" 
+
+        return content
+    }
+
     #renderBody() {
         this.#data.forEach((row, index) => {
             let rowElem = Component.createElement({
+                element: this.#expand ? "summary" : "div",
                 classAttr: "grid-row"
             })
 
             for (let column of this.#columns) {
                 rowElem.appendChild(Component.createElement({
                     classAttr: "grid-cell",
-                    content: column.content ? column.content(row, index) : row[column.key] ?? "&mdash;"
+                    content: this.#getCellContent(index, row, column)
                 }))
             }
 
-            this.#body.appendChild(rowElem)
+            if (this.#expand && this.#expand(row)) {
+                const DETAILS = Component.createElement({
+                    element: "details"
+                })
+                DETAILS.appendChild(rowElem)
+                DETAILS.appendChild(Component.createElement({
+                    classAttr: "grid-expand",
+                    content: this.#expand(row)
+                }))
+                this.#body.appendChild(DETAILS)
+            }
+            else this.#body.appendChild(rowElem)
         })
     }
 
